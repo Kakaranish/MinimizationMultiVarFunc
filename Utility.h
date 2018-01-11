854a2728903ec97bed67f08ba721aed812e3db78
Kakaranish <stasiekgruz@gmail.com>
2018-01-11 21:55:45 +0100

    Hello world init

--- Utility.h
+++ Utility.h
@@ -2,5 +2,3 @@
 #include "Libs.h"
 #include "Matrix.h"
 
-std::pair<double, Matrix*> computeLeadingEigenpair(Matrix* matrix, int steps);
-void computeEigenpairs(Matrix*, int steps);
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+#include "Libs.h"
+#include "Utility.h"
+
+int main(int argc, char* argv[]) {
+
+	std::cout << "Hello world " << std::endl;
+
+
+	return 0;
+}
